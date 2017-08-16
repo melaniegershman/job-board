a5a9c30002c7e3cbfcf099c443837b932e36e228
Melanie Gershman <melanie.gershman@gmail.com>
2017-08-15 21:18:31 -0400

    Seed db

--- job-board/db/seeds.rb
+++ job-board/db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,12 +9,11 @@
 experience = ["Entry-level", "Senior", "Mid-level", "Intern"]
 willingness = [true, false]
 
-jobs = []
-10.times do {
-  jobs.push(Job.create([
-    {name: Faker::job.title},
-    {description: Faker::Lorem.paragraph},
-    {experience: experience.sample},
-    {willing_to_relocate: willingness.sample}
-  ]))
-}
+jobs = 10.times.map do
+  Job.create!(
+    :name => Faker::Company.profession,
+    :description => Faker::Lorem.paragraph,
+    :experience => experience.sample,
+    :willing_to_relocate => willingness.sample
+  )
+end
