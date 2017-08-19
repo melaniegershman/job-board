46da84c3ae803a8e60e3236f9e1a4d02b5a8784a
Melanie Gershman <melanie.gershman@gmail.com>
2017-08-18 23:08:42 -0400

    User can submit a new job posting

--- job-board-client/src/components/form.js
+++ job-board-client/src/components/form.js
@@ -1,25 +1,82 @@
 import React, { Component } from 'react';
+import { Redirect, Link } from 'react-router-dom';
 
 const config = {
-
+    name: null,
+    description: null,
+    experience: null,
+    willing_to_relocate: null
 }
 
-const JobPostForm = () => {
-    return (
-            <div>
-                <p>Job Title:</p>
-                <input placeholder='Enter the title of the job position' />
-                <p>Job Description:</p>
-                <input placeholder='Enter a job description' />
-                <p>Is this position remote?</p>
-                <input type="radio" value="yes"/>
-                <label>Yes</label>
-                <input type="radio" value="no"/>
-                <label>No</label>
-                <button onClick={this.handleClick}>Submit</button>
-            </div>
+class JobPostForm extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            name: '',
+            description: '',
+            experience: '',
+            willing_to_relocate: ''
+        }
 
-    )
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const newState = {}
+        newState[event.target.name] = event.target.value;
+
+        this.setState(newState);
+
+        console.log('handleChange: ', event.target.value);
+
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var details = this.state;
+
+        var formBody = [];
+        for (var property in details) {
+          var encodedKey = encodeURIComponent(property);
+          var encodedValue = encodeURIComponent(details[property]);
+          formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
+
+        fetch('http://localhost:3000/api/v1/jobs', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: formBody
+      }).then(response => response.json()).then(response => console.log("response: ", response ))
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <p>Job Title:</p>
+                <input type="text" name="name" placeholder='Enter the title of the job position' value={this.state.name} onChange={this.handleChange} />
+                <p>Job Description:</p>
+                <textarea rows="4" cols="50" name="description" placeholder='Enter a job description' value={this.state.description} onChange={this.handleChange}> </textarea>
+                <p>What is your required experience level?</p>
+                <select name="experience" onChange={this.handleChange}>
+                  <option value="Intern">Intern</option>
+                  <option value="Entry-level">Entry-level</option>
+                  <option value="Mid-level">Mid-level</option>
+                  <option value="Senior">Senior</option>
+                </select>
+                <p>Are you willing to relocate?</p>
+                <input type="radio" name="willing_to_relocate" value="true" onChange={this.handleChange} />
+                <label>Yes</label>
+                <input type="radio" name="willing_to_relocate" value="false" onChange={this.handleChange} />
+                <label>No</label>
+                <input type="submit"/>
+            </form>
+        )
+    }
 }
 
 
--- job-board-client/src/components/jobs.js
+++ job-board-client/src/components/jobs.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
-const Job = props => {
+export class JobListings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            jobs: []
+        };
+    }
+
+    loadData() {
+        fetch('http://localhost:3000/api/v1/jobs.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    jobs: data
+                });
+            });
+            // need to add a catch for error
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+
+    render() {
+        if(this.state.jobs) {
+            let jobs = this.state.jobs.map( job => {
+                var willing_to_relocate = (job.willing_to_relocate) ? "Yes" : "No";
+                return (
+                    <Job
+                        key={job.id}
+                        name={job.name}
+                        description={job.description}
+                        experience={job.experience}
+                        willing_to_relocate={willing_to_relocate}
+                    />
+                );
+            });
+
+            return (
+                <div>
+                    <Link to='/jobs/new'>Add New Job Posting</Link>
+                    {jobs}
+                </div>
+            )
+
+        } else {
+            return <div></div>
+        }
+    }
+}
+
+
+
+export const Job = props => {
     return (
         <li>
-            <h3>Title: {props.name}</h3>
-            <p>Description: {props.description}</p>
+            <h3>Job Title: {props.name}</h3>
+            <p>Job Description: {props.description}</p>
             <p>Experience required: {props.experience}</p>
+            <p>Willing to relocate: {props.willing_to_relocate}</p>
         </li>
     )
 }
-
-export default Job;
